750c9887db29ba5ee07edbe1c6618b6988e85e38
MUHAMMED SÖNMEZ <120255174+sonmez1249@users.noreply.github.com>
2024-12-12 14:28:15 +0300

    Update servis.py

--- servis.py
+++ servis.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Sayfa yapılandırması (ikon eklendi)
 st.set_page_config(
     page_title="İletişim Bilgileri",
-    page_icon="C:\\Users\\omera\\Desktop\\TradeMentorAi\\streamlit_TradeMentorAi\\images\\Leonardo_Phoenix_Create_a_modern_sleek_logo_for_the_stock_trad_2.jpg",  # İkon dosya yolu
+    
     layout="wide",
     initial_sidebar_state="expanded"
 )
